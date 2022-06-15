289caf76f12f63040df1fe52a5cf4ce0533b555a
NicholasHabryl <84864635+NicholasHabryl@users.noreply.github.com>
2022-06-14 20:41:18 -0400

    Menu now friendly to serial inputs

--- menu.py
+++ menu.py
@@ -8,8 +8,23 @@ import games.mindthegap
 import games.speedrun
 import utils
 
-#root =tk.Tk()
-#SCREEN_DIMENSIONS = (root.winfo_screenwidth(), root.winfo_screenheight()) #full screen
+from pygame.locals import ( #many of these K_whatevers can be removed once out of testing
+    K_a,
+    K_d,
+    K_z,
+    K_c,
+    K_w,
+    K_x,
+    K_e,
+    K_q,
+    K_RETURN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
 SCREEN_DIMENSIONS = (800, 600)
 
 MODES = [
@@ -44,9 +59,14 @@ def updatenum(value, num: int):
     utils.FOCUS_NUM[0] = num
     print(utils.MODE[0])
 
+def on_resize():
+    window_size = surface.get_size()
+    menu.resize(window_size[0], window_size[1])
+    print(f'New menu size: {menu.get_size()}')
+
 pygame.init()
 
-surface = pygame.display.set_mode(SCREEN_DIMENSIONS)
+surface = pygame.display.set_mode(SCREEN_DIMENSIONS,pygame.RESIZABLE)
 
 #defining menus
 menu = pm.Menu( title="Main Menu",
@@ -191,5 +211,39 @@ menu.add.button("Practice mode",pracmodemenu)
 menu.add.button("Leaderboards")
 menu.add.button("Exit",pm.events.EXIT)
 
-menu.mainloop(surface)
+#this is what initially worked
+#menu.mainloop(surface)
 
+if __name__ == '__main__':
+    while True:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                break
+            if event.type == pygame.VIDEORESIZE:
+                # Update the surface
+                surface = pygame.display.set_mode((event.w, event.h),
+                                                  pygame.RESIZABLE)
+                # Call the menu event
+                on_resize()
+
+            #these statements can be modified once serial input can be read
+            if event.type == KEYDOWN:
+
+                if event.key == K_w:
+                    pygame.event.post(pygame.event.Event(pygame.KEYDOWN,key=pygame.K_RETURN))
+                if event.key == K_a:
+                    pygame.event.post(pygame.event.Event(pygame.KEYDOWN,key=pygame.K_LEFT))
+                if event.key == K_d:
+                    pygame.event.post(pygame.event.Event(pygame.KEYDOWN,key=pygame.K_RIGHT))
+                if event.key == K_x:
+                    pygame.event.post(pygame.event.Event(pygame.KEYDOWN,key=pygame.K_BACKSPACE))
+
+        # Draw the menu
+        surface.fill((25, 0, 50))
+
+        menu.update(events)
+        menu.draw(surface)
+
+        pygame.display.flip()
